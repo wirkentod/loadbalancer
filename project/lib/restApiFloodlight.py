6b1f8ee7f978cc84728b19e120d42093056b75c1
Christian Quispe <wirkentod@gmail.com>
2017-05-31 21:02:41 -0500

    measureFlows

--- project/lib/restApiFloodlight.py
+++ project/lib/restApiFloodlight.py
@@ -23,3 +23,22 @@ class RestApiFloodlight(object):
 	#print ret[2]
         conn.close()
         return enviar
+
+    def measureFlows(self, ovsPid):
+	path = '/wm/core/switch/'+ ovsPid +'/flow/json'
+        #print path
+	headers = {
+            'Content-type': 'application/json',
+            'Accept': 'application/json',
+        }
+        body = json.dumps("")
+        conn = httplib.HTTPConnection(self.server, 8080)
+        conn.request('GET', path)
+	#conn.request('GET', path, body, headers)
+        response = conn.getresponse()
+        ret = (response.status, response.reason, response.read())
+        enviar = ret[2]
+	#print ret[2]
+        conn.close()
+        return enviar
+
