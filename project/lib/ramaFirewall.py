c34ac37ba05715d6dc64f80d7279553ffd4635aa
Christian Quispe <wirkentod@gmail.com>
2017-06-04 16:45:02 -0500

    agregar atributos RamaFirewall

--- project/lib/ramaFirewall.py
+++ project/lib/ramaFirewall.py
@@ -1,12 +1,13 @@
 class RamaFirewall(object):
 	carga_ovs_intranet = 0
 	carga_ovs_extranet = 0
-	def __init__(self, interfaz_puerto_ovs_intranet, interfaz_puerto_ovs_extranet, ramaFirewallNombre, estado, flagtmp):
+	def __init__(self, interfaz_puerto_ovs_intranet, interfaz_puerto_ovs_extranet, ramaFirewallNombre, estado, flagtmp, SubRedes):
 		self.interfaz_puerto_ovs_intranet = interfaz_puerto_ovs_intranet
 		self.interfaz_puerto_ovs_extranet = interfaz_puerto_ovs_extranet
 		self.ramaFirewallNombre = ramaFirewallNombre
 		self.estado = estado
 		self.flagtmp = flagtmp
+		self.SubRedes = SubRedes
 	def carga_representativa(self):
 		return max([self.carga_ovs_intranet,self.carga_ovs_extranet])
 	
