d264638dcf0bcc0e1dada696fee1c3566923e66a
Christian Quispe <wirkentod@gmail.com>
2017-06-16 19:38:14 -0500

    groupId

--- project/lib/ramaFirewall.py
+++ project/lib/ramaFirewall.py
@@ -1,9 +1,10 @@
 class RamaFirewall(object):
 	carga_ovs_intranet = 0
 	carga_ovs_extranet = 0
-	def __init__(self, interfaz_puerto_ovs_intranet, interfaz_puerto_ovs_extranet, ramaFirewallNombre, estado, flagtmp, SubRedes):
+	def __init__(self, interfaz_puerto_ovs_intranet, interfaz_puerto_ovs_extranet, groupID_ovs_intranet, ramaFirewallNombre, estado, flagtmp, SubRedes):
 		self.interfaz_puerto_ovs_intranet = interfaz_puerto_ovs_intranet
 		self.interfaz_puerto_ovs_extranet = interfaz_puerto_ovs_extranet
+		self.groupID_ovs_intranet = groupID_ovs_intranet
 		self.ramaFirewallNombre = ramaFirewallNombre
 		self.estado = estado
 		self.flagtmp = flagtmp
