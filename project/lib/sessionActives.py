e3d0580999c8877c2dd1901454e7c9c40e8d9c2a
Christian Quispe <wirkentod@gmail.com>
2017-06-16 20:04:12 -0500

    sessions

--- project/lib/sessionActives.py
+++ project/lib/sessionActives.py
@@ -11,7 +11,7 @@ class SessionActives(object):
         	headers = {
 			'Content-type': 'application/json',
 			'Accept': 'application/json',
-			}
+		}
 		body = json.dumps("")
 		conn = httplib.HTTPConnection(self.server, 8080)
 		conn.request('GET', path)
@@ -27,7 +27,7 @@ class SessionActives(object):
         	headers = {
 			'Content-type': 'application/json',
 			'Accept': 'application/json',
-			}
+		}
 		body = json.dumps("")
 		conn = httplib.HTTPConnection(self.server, 8080)
 		conn.request('GET', path)
