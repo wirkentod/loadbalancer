168071cbeb7d4a20e1ca609219da5d05245cec62
Christian Quispe <wirkentod@gmail.com>
2017-06-28 18:34:22 -0500

    complete

--- project/main.py
+++ project/main.py
@@ -294,7 +294,112 @@ def accionCadaXSegundos():
 				rama.temp_bloqueado = rama.temp_bloqueado - 1
 		
 		print "Rama Nombre: %s| carga_representativa: %s| Rama estado: %s| Rama flagtmp: %s | SubRedes: %s " %(rama.ramaFirewallNombre, rama.carga_representativa(), rama.estado, rama.flagtmp, rama.SubRedes)
-		
+	
+	#Ordenamos los arreglos donde se almacenas las ramas 'ESTABLES' E 'INESTABLES' de forma descendente
+	ramas_HandOff_src_new_descendente = sorted(ramas_HandOff_src_new, key=lambda rama: rama.carga_representativa(), reverse=True) 
+	ramas_HandOff_dst_new_ascendente = sorted(ramas_HandOff_dst_new, key=lambda rama: rama.carga_representativa(), reverse=False)
+	
+	i = 0
+	cantidad_ramas_HandOff_dst = len(ramas_HandOff_dst_new_ascendente)
+	
+	for rama in ramas_HandOff_src_new_descendente:
+		if rama.estado != 'BLOQUEADO':
+			if rama.estado == 'NORMAL' :
+				#Buscamos la SubRed que más se acomada para realizar el HandOff, ordenamos de manera ascendente
+				SubRedes_ramas_ascendente = sorted(rama.SubRedes, key=lambda subred: subred.bps, reverse=False)
+				#Escogemos la subred de menor carga para evitar el amortiguamiento del sistema
+				sub_red_elegida = SubRedes_ramas_ascendente[0]
+				rama.SubRedHO = sub_red_elegida
+				#Cambiamos la forma del prefijo a formato ip/num
+				input = sub_red_elegida.ip_mask
+				res = input.split('/')
+				output_prefix = res[0] + "/" + change_mask(res[1])
+				subredparams = sub_red_elegida.nombre + '&' + output_prefix
+				#Insertamos un Flow Entry para generar un port mirror de la SubRed correspondiente
+				flow_mirror = {
+						'switch':ovs_intranet_DPID,
+						"name":str(subRedName) + "_mirror",
+						"cookie":"0",
+						"priority":"20",
+						"eth_type ":"0x0800",
+						"ipv4_src":sub_red_elegida.ip_mask,
+						"active":"true",
+						"in_port":str(interfaz_default_ovs_intranet),
+						"hard_timeout": "15",
+						"actions":"group=" + str(rama.groupID_ovs_intranet)
+						}
+				pusher.set(flow_mirror)
+				#Empieza la recoleccion de las sesiones activas
+				rpta = sessionActives.startCollection(subredparams)
+				rama.estado = 'CANDIDATA'
+				rama.timeHOsessions = time.time()
+				#Observacion, Agregar a la clase RamaFirewall los siguietnes atributos: self.temp_HandOff y self.temp_bloqueado y self.SubRedHO, self.timeHOsessions
+				#rama.temp_HandOff = 5
+			elif rama.estado == 'CANDIDATA' :
+				if time.time() - rama.timeHOsessions >= 15 :
+				
+					indice = i % cantidad_ramas_HandOff_dst
+					
+					input = rama.SubRedHO
+					res = input.split('/')
+					output_prefix = res[0] + "/" + change_mask(res[1])
+					subredparams = rama.SubRedHO.nombre + '&' + output_prefix
+					#query para importar informacion de las sesiones activas
+					data = sessionActives.getSessions(subredparams)
+					session_dict = ast.literal_eval(data)
+					#Eliminamos el objeto que recolecta sesiones
+					rpta = sessionActives.stopSearch(subredparams)
+					
+					#Ejecutamos las reglas de HandOff para las sesiones activas
+					ejecutarFlowEntriesSessionesActivas(rama,session_dict)
+					
+					#Ejecutamos el HO de la subRED, escogemos el triplet
+					#Triplet:
+					Rama_src = rama
+					#No se considera el caso cuando ramas_HandOff_dst_new_ascendente[] esta vacio
+					#La eleccion del destino es en funcion al Round-Robin del arreglo ramas_HandOff_dst_new_ascendente 
+					Rama_dst = ramas_HandOff_dst_new_ascendente[indice]
+					Sub_red_HO = rama.SubRedHO
+					
+					#HandOff de sub-red a la Rama_dst
+					flowSubNet_intranet = {
+						'switch':ovs_intranet_DPID,
+						"name":str(Sub_red_HO.nombre) + "_intranet",
+						"cookie":"0",
+						"priority":"10",
+						"eth_type ":"0x0800",
+						"ipv4_src":str(Sub_red_HO.ip_mask),
+						"active":"true",
+						"in_port":str(interfaz_default_ovs_intranet),
+						"actions":"output=" + str(Rama_dst.interfaz_puerto_ovs_intranet)
+						}
+
+					flowSubNet_extranet = {
+						'switch':ovs_extranet_DPID,
+						"name":str(Sub_red_HO.nombre) + "_extranet",
+						"cookie":"0",
+						"priority":"10",
+						"eth_type ":"0x0800",
+						"ipv4_dst":str(Sub_red_HO.ip_mask),
+						"active":"true",
+						"in_port":str(interfaz_default_ovs_extranet),
+						"actions":"output=" + str(Rama_dst.interfaz_puerto_ovs_extranet)
+						}
+
+					pusher.set(flowSubNet_intranet)
+					pusher.set(flowSubNet_extranet)
+					
+					#Actualizamos las subredes de las ramas
+					Rama_src.SubRedes.remove(Sub_red_HO)
+					Rama_dst.SubRedes.append(Sub_red_HO)
+					
+					rama.estado = 'BLOQUEADO'
+					
+					#Reseteamos la subred en la busqueda de sesiones
+					sessionActives.stopSearch(output)
+					rama.temp_bloqueado = 5
+					i = i + 1
+				else:
 
 if __name__ == '__main__':
 	#Creacion de Group entries para realizar forwarding multicast para recolectar las sesiones activas por Sub Red
