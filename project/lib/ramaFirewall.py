a1da9cca157ddc1c301b2230e3eb3d0aff2653de
Christian Quispe <wirkentod@gmail.com>
2017-07-05 12:38:50 -0500

    promediador

--- project/lib/ramaFirewall.py
+++ project/lib/ramaFirewall.py
@@ -13,6 +13,7 @@ class RamaFirewall(object):
 		self.flagtmp_old = ""
 		self.temp_bloqueado = ""
 		self.timeHOsessions = ""
+		self.promediador_old = 0
 	def carga_representativa(self):
 		return max([self.carga_ovs_intranet,self.carga_ovs_extranet])
 	
